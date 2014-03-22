6d441965b111d3a9b03148d1702b9dc111a94981
Feross Aboukhadijeh <feross@feross.org>
2014-03-21 17:37:15 -0700

    fix exports style

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @param {number} [i=1] - Number of beeps
  * @param {number} [t=500] - Milliseconds between beeps
  */
-exports = module.exports = function beep(i, t) {
+module.exports = function (i, t) {
   if (isNaN(i)) i = 1
   if (isNaN(t)) t = 500
 
