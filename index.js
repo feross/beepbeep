d1355001607d6ad69c7329ba0f5e89c206ba5824
Feross Aboukhadijeh <feross@feross.org>
2014-03-21 17:37:33 -0700

    first beep should be synchronous

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ module.exports = function (i, t) {
   if (isNaN(i)) i = 1
   if (isNaN(t)) t = 500
 
-  while (i-- > 0) setTimeout(beepNow, t*i)
+  while (i-- > 0) {
+    if (t*i === 0) beepNow()
+    else setTimeout(beepNow, t*i)
+  }
 }
 
 function beepNow() { process.stdout.write('\x07') }
