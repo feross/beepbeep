e8701d79aed3de357dd2402f122a54fa4c8f9b4f
Manuel Spigolon <behemoth89@gmail.com>
2019-04-11 19:05:08 +0200

    add: cli interface

---
+++ cli.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+'use strict'
+
+var beep = require('./index')
+
+try {
+  var args = process.argv.splice(2).map(_ => parseInt(_))
+  switch (args.length) {
+    case 0:
+      beep()
+      break
+    case 1:
+      beep(args[0])
+      break
+    case 2:
+      beep(args[0], args[1])
+      break
+    default:
+      beep(args)
+      break
+  }
+} catch (error) {
+  console.log('Wrong parameters', error)
+  process.exit(-1)
+}
